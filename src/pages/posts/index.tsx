b013ae622a62c954ddad2d968f8773fd04d96394
olliepayne <olliepayne@me.com>
2021-07-26 15:40:19 -0700

    Begin working on query for local JSON posts data;

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,9 +1,12 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
 import React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../../components/Layout'
 
-const PostsIndexPage = () => {
+const PostsIndexPage = ({ data }) => {
+  console.log(data)
+
   return (
     <Layout>
       <main
@@ -16,7 +19,18 @@ const PostsIndexPage = () => {
             
           }}
         >
-          All posts
+          {/* <ul
+            sx={{
+              p: '0px',
+              listStyleType: 'none'
+            }}
+          >
+            {data.map(({ title }) => (
+              <li>
+                <h4>{title}</h4>
+              </li>
+            ))}
+          </ul> */}
         </h1>
       </main>
     </Layout>
@@ -24,3 +38,11 @@ const PostsIndexPage = () => {
 }
 
 export default PostsIndexPage
+
+export const query = graphql`
+  {
+    file(name: {eq: "posts"}) {
+      name
+    }
+  }
+`
